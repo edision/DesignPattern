22cce116c3d5d02093e1f739bbd01954f4b13dcf
吴剑超 <boywujch@hotmail.com>
2017-08-09 00:56:33 +0800

    单件模式完整实例

---
+++ 01-Singleton/SingletonNested.cs
@@ -0,0 +1,35 @@
+﻿// 创建人       吴剑超
+// 创建时间 2017-08-09 0:21
+namespace Singleton
+{
+    /// <summary>
+    /// 延迟加载支持
+    /// 使用内部类初始化，可以调用非默认构造函数或执行其他步骤。
+    /// </summary>
+    public sealed class SingletonNested
+    {
+        private int count = 0;
+
+        SingletonNested()
+        {
+        }
+
+        SingletonNested(int count)
+        {
+            this.count = count;
+        }
+
+        public static SingletonStatic Instance => Nested.instance; // 调用的时候才实例化，实现了延迟加载
+
+        private class Nested
+        {
+            static Nested()
+            {
+                //
+            }
+
+            internal static readonly SingletonStatic instance = new SingletonStatic();
+        }
+    }
+    
+}
